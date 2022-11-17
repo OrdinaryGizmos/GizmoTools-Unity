4d6ca4e25bc6aead089930c50ec2dcc3fb05f2aa
Jonny Kenyon <J.Kenyon@ordinarygizmos.com>
2022-11-17 10:42:52 -0600

    Add License Meta file to remove warnings and allow for Custom Editor of DialogueSO

--- Runtime/DialogueSystem/Scripts/ScriptableObjects/DSDialogueSO.cs
+++ Runtime/DialogueSystem/Scripts/ScriptableObjects/DSDialogueSO.cs
@@ -8,11 +8,13 @@ namespace OrdinaryGizmos.GizmoTools.DS.ScriptableObjects
 
     public class DSDialogueSO : ScriptableObject
     {
-        [field: SerializeField] public string DialogueName { get; set; }
-        [field: SerializeField] [field: TextArea()] public string Text { get; set; }
-        [field: SerializeField] public List<DSDialogueChoiceData> Choices { get; set; }
-        [field: SerializeField] public DSDialogueType DialogueType { get; set; }
-        [field: SerializeField] public bool IsStartingDialogue { get; set; }
+        [field: SerializeField] public string DialogueName;// { get; set; }
+        [field: SerializeField][field: TextArea()] public string Text;// { get; set; }
+        [field: SerializeField] public List<DSDialogueChoiceData> Choices;// { get; set; }
+        [field: SerializeField] public DSDialogueType DialogueType;// { get; set; }
+        [field: SerializeField] public bool IsStartingDialogue;// { get; set; }
+        [field: SerializeField] public UnityEngine.Events.UnityEvent StartEvents;// { get; set; }
+        [field: SerializeField] public UnityEngine.Events.UnityEvent EndEvents;// { get; set; }
 
         public void Initialize(string dialogueName, string text, List<DSDialogueChoiceData> choices, DSDialogueType dialogueType, bool isStartingDialogue)
         {
@@ -21,6 +23,7 @@ namespace OrdinaryGizmos.GizmoTools.DS.ScriptableObjects
             Choices = choices;
             DialogueType = dialogueType;
             IsStartingDialogue = isStartingDialogue;
+            StartEvents?.Invoke();
         }
     }
 }
